da9b485b77dec26a818b589a43d92c2e49204c91
AbdurRehmanMalik1 <abdurrehman4415@gmail.com>
2025-04-19 18:04:20 +0500

    Modified Login Css

--- src/views/Login.tsx
+++ src/views/Login.tsx
@@ -1,18 +1,17 @@
-import React from 'react'
-
 type Props = {}
 
 const Login = (props: Props) => {
+    const inputClass : string = 'w-80 px-4 py-2 rounded-[8px] shadow-sm outline-none';
     return (
-        <div>
-            <div className='w-[500px] flex flex-col gap-y-4 items-center'>
-                <h1>Compawnion</h1>
-                <input placeholder='Email Address' />
-                <input placeholder='Password' />
-                <div className='flex flex-row justify-between'>
-                    <button>Login</button>
-                    <div>
-                        <input type="checkbox" />
+        <div className="flex flex-col">
+            <div className='mt-20 flex flex-col gap-y-10 m-auto'>
+                <h1 className="text-4xl text-center font-medium">Compawnion</h1>
+                <input className={inputClass} placeholder='Email Address' />
+                <input className={inputClass} placeholder='Password' />
+                <div className='flex flex-row justify-between items-center'>
+                    <button className="bg-red-100 rounded-[8px] px-6 py-2">Login</button>
+                    <div className="flex gap-x-2">
+                        <input type='checkbox' />
                         <label>Keep me logged in</label>
                     </div>
                 </div>
