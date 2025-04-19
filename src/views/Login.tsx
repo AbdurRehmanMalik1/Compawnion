c635d1022938c97ba46671c2180b3261e68b5c4c
AbdurRehmanMalik1 <abdurrehman4415@gmail.com>
2025-04-19 18:30:41 +0500

    Modified Login Image

--- src/views/Login.tsx
+++ src/views/Login.tsx
@@ -1,21 +1,23 @@
 type Props = {}
 
+const inputClass : string = 'w-100 px-4 py-2 rounded-[8px] shadow-sm outline-none';
 const Login = (props: Props) => {
-    const inputClass : string = 'w-80 px-4 py-2 rounded-[8px] shadow-sm outline-none';
     return (
-        <div className="flex flex-col">
-            <div className='mt-20 flex flex-col gap-y-10 m-auto'>
+        <div className="flex flex-row h-screen">
+            <div style={{background:'black',width:'50%'}}>
+            </div>
+            <form className="mt-20 flex flex-col gap-y-20 m-auto">
                 <h1 className="text-4xl text-center font-medium">Compawnion</h1>
-                <input className={inputClass} placeholder='Email Address' />
-                <input className={inputClass} placeholder='Password' />
-                <div className='flex flex-row justify-between items-center'>
-                    <button className="bg-red-100 rounded-[8px] px-6 py-2">Login</button>
+                <input className={inputClass} type="email" placeholder='Email Address' />
+                <input className={inputClass} type="password" placeholder='Password' />
+                <div className="flex flex-row justify-between items-center">
+                    <button type="submit" className="bg-red-100 rounded-[8px] px-6 py-2">Login</button>
                     <div className="flex gap-x-2">
-                        <input type='checkbox' />
-                        <label>Keep me logged in</label>
+                        <input id="keepLogin" name="keepLogin" type="checkbox" />
+                        <label htmlFor="keepLogin">Keep me logged in</label>
                     </div>
                 </div>
-            </div>
+            </form>
 
         </div>
     )
