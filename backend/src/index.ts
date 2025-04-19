51d20793ed63dd5fd074e96abb92e68e84634b59
AbdurRehmanMalik1 <abdurrehman4415@gmail.com>
2025-04-20 00:33:01 +0500

    Intialzed Express Server

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,15 @@
 import express, { Request, Response } from 'express';
+import dotenv from 'dotenv'
+import cors from 'cors';
+
+dotenv.config()
 
 const app = express();
 const PORT : string = process.env.PORT || '3000';
+
+
+app.use(cors()); 
+app.use(express.json()); 
 
 app.get('/', (req: Request, res: Response) => {
   res.json({ message: 'Hello from Express + TypeScript!' });
